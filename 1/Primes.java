3893cda7258327e02b9a062eb20f027accb44808
galion0504-star <galion0504@gmail.com>
2026-05-30 21:51:45 +0300

    Create Primes.java

---
+++ 1/Primes.java
@@ -0,0 +1,21 @@
+public class Primes {
+    public static void main(String[] args) {
+        for (int num = 1; num <= 100; num++) {
+            if (isPrime(num)) {
+                System.out.println(num);
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i*i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
